26be3f52a276233346f39164ebeb0e2cb8f9db32
Pratul11 <110842217+Pratul11@users.noreply.github.com>
2023-02-06 10:41:29 +0530

    update1

--- search1.c
+++ search1.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello World!");
-    return 0;
+    int n,search,code=0;
+    printf("Enter Size of an array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter %d numbers\n",n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter number to be search: ");
+    scanf("%d",&search);
+    for(int i=0;i<n;i++)
+    {
+        if(search==arr[i])
+        {
+            printf("Number Found!\n");
+            printf("Position: %d",i+1);
+            code++;
+            break;
+        }
+        
+    }
+    if(code==0)
+    {
+        printf("Number not found!");
+    }
 }
